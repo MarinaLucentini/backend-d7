68f0c1e1a6ad0185a751e69957a5a3ac81e57d0c
MARINA MARIA LUCENTINI <158228930+MarinaLucentini@users.noreply.github.com>
2024-05-28 15:12:10 +0200

    esercizio 1

---
+++ src/es1/Esercizio1.java
@@ -0,0 +1,31 @@
+package es1;
+
+import java.util.HashSet;
+import java.util.Scanner;
+import java.util.Set;
+
+public class Esercizio1 {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Indicami con un numero intero quante parole vuoi inserire");
+        int numeroSet = Integer.parseInt(scanner.nextLine());
+        Set<String> textduplicatiSet = new HashSet<>();
+        Set<String> textSet = new HashSet<>();
+
+        for (int i = 0; i < numeroSet; i++) {
+            System.out.println("Scrivimi le parole che vuoi inserire");
+            String text = scanner.nextLine();
+            if (textSet.add(text)) {
+                textSet.add(text);
+            } else {
+                textduplicatiSet.add(text);
+            }
+
+        }
+        System.out.println(textSet);
+        System.out.println(textSet.size());
+        System.out.println(textduplicatiSet);
+
+
+    }
+}
